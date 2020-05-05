b2f25889783f0119f9c21eb0659f2ae9a607dd60
cyriacbijun <cyriacbijun@gmail.com>
2020-05-05 15:50:31 +0530

    Initial commit

---
+++ android/app/src/main/java/com/cerebro/destini/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cerebro.destini;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story{
+  String storyTitle,storyChoice1,storyChoice2;
+  Story({String storyTitle,String choice1,String choice2}){
+    storyTitle = storyTitle;
+    storyChoice1 = choice1;
+    storyChoice2 = choice2;
+  }
+}
+
+
