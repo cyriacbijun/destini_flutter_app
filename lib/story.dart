31e3940a5f31be2b8fadbd57c3e56c156787ee28
cyriacbijun <cyriacbijun@gmail.com>
2020-05-06 12:19:14 +0530

    Made the app upto changing _storyNumber according to choiceNumber

--- lib/story.dart
+++ lib/story.dart
@@ -1,7 +1,7 @@
 class Story{
-  String storyTitle,storyChoice1,storyChoice2;
+  String storyTitleName,storyChoice1,storyChoice2;
   Story({String storyTitle,String choice1,String choice2}){
-    storyTitle = storyTitle;
+    storyTitleName = storyTitle;
     storyChoice1 = choice1;
     storyChoice2 = choice2;
   }
